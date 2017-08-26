4a514e6e2e1ded6433f555b0f0313ecda061b88a
Majid Abdolhosseini <majid8911303@gmail.com>
2017-08-26 13:20:55 +0430

    add needed fields to address value object

--- src/implementations/Address.php
+++ src/implementations/Address.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace mhndev\valueObjects\implementations;
 
 use mhndev\valueObjects\interfaces\iValueObject;
@@ -41,6 +40,11 @@ final class Address implements iValueObject
      */
     protected $no;
 
+    /**
+     * @var Point
+     */
+    protected $point;
+
 
     /**
      * Address constructor.
@@ -50,9 +54,18 @@ final class Address implements iValueObject
      * @param string $street
      * @param string $block
      * @param string $no
-     * @param string|null $postalCode
+     * @param string | null $postalCode
+     * @param null $point
      */
-    public function __construct($country, $city, $street, $block, $no, $postalCode = null)
+    public function __construct(
+        $country,
+        $city,
+        $street,
+        $block,
+        $no,
+        $postalCode = null,
+        $point = null
+    )
     {
         $this->country = $country;
         $this->city = $city;
@@ -60,7 +73,26 @@ final class Address implements iValueObject
         $this->block = $block;
         $this->no = $no;
         $this->postalCode = $postalCode;
+        $this->point = $point;
+    }
 
+
+
+    /**
+     * @param array $options
+     * @return static
+     */
+    static function fromOptions(array $options)
+    {
+        return new static(
+            $options['country'],
+            $options['city'],
+            $options['street'],
+            $options['block'],
+            $options['no'],
+            $options['postalCode'],
+            $options['point']
+        );
     }
 
 
@@ -92,4 +124,82 @@ final class Address implements iValueObject
         /** @var Address $valueObject */
         return ($this->__toString() == $valueObject->__toString());
     }
+
+
+
+    /**
+     * @return string
+     */
+    public function getPostalCode(): string
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlock(): string
+    {
+        return $this->block;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNo(): string
+    {
+        return $this->no;
+    }
+
+    /**
+     * @return Point
+     */
+    public function getPoint(): Point
+    {
+        return $this->point;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            'country'    => $this->getCountry(),
+            'city'       => $this->getCity(),
+            'block'      => $this->getBlock(),
+            'no'         => $this->getNo(),
+            'postalCode' => $this->getPostalCode(),
+            'point'      => $this->getPoint(),
+            'street'     => $this->getStreet()
+        ];
+
+    }
+
 }
